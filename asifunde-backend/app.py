c9da3407841691e4bf5a0baecc9056353318edaa
Soshtakovich <zakessamson1@gmail.com>
2024-09-20 20:22:13 +0200

    Day_6

--- asifunde-backend/app.py
+++ asifunde-backend/app.py
@@ -4,6 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+# Home route for '/'
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'This is the homepage of the Asifunde API!'})
+
+# API route for '/api'
 @app.route('/api', methods=['GET'])
 def home():
     return jsonify({'message': 'Welcome to the Asifunde API!'})
